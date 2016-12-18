c2c65ce5169e25eb616d5377a5b8abcdc8f15b0a
Michael Ushakov <um.nix.user@gmail.com>
2016-12-18 20:07:14 +0400

    ClientInfo plus server base api

---
+++ TinyTcpServer/TinyTcpServer.Api/Client/TcpClientInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TinyTcpServer.Api.Client
+{
+    public class TcpClientInfo
+    {
+        public TcpClientInfo(String ipAddress, Int32 port = -1)
+        {
+            IpAddress = ipAddress;
+            Port = port;
+        }
+
+        public String IpAddress { get; private set; }
+        public Int32 Port { get; private set; }
+    }
+}
---
+++ TinyTcpServer/TinyTcpServer.Api/Server/ITcpServer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TinyTcpServer.Api.Client;
+
+namespace TinyTcpServer.Api.Server
+{
+    public interface ITcpServer
+    {
+        Boolean Start(String ipAddress, UInt16 port);
+        void Stop();
+        void Restart();
+        // handler functions
+        void AddHandler(TcpClientInfo clientInfo, Func<Byte[], TcpClientInfo, Byte[]> handler);
+        void RemoveHandler(TcpClientInfo clientInfo);
+        // send data functions
+        void SendData(TcpClientInfo clientInfo, Byte[] data);
+        // filtering functions
+    }
+}
