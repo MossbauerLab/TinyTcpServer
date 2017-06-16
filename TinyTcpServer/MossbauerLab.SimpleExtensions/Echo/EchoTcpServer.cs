192e08e47161997cd9d0513f813b512e4c360b9a
Michael Ushakov <um.nix.user@gmail.com>
2017-06-16 16:31:46 +0400

    Was added Echo server extension

---
+++ TinyTcpServer/MossbauerLab.SimpleExtensions/Echo/EchoTcpServer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using log4net;
+using MossbauerLab.TinyTcpServer.Core.Handlers;
+using MossbauerLab.TinyTcpServer.Core.Server;
+
+namespace MossbauerLab.SimpleExtensions.Echo
+{
+    class EchoTcpServer : TcpServer
+    {
+        private static class EchoTcpClientHandler
+        {
+            public static Byte[] Handle(Byte[] data, TcpClientHandlerInfo info)
+            {
+                lock (data)
+                {
+                    Byte[] outputData = new Byte[data.Length];
+                    Array.Copy(data, outputData, data.Length);
+                    return outputData;
+                }
+            }
+        }
+
+        public EchoTcpServer(String ipAddress, UInt16 port = DefaultEchoPort, ILog logger = null, Boolean debug = false)
+            : base(ipAddress, port, logger, debug)
+        {
+            AddHandler(new TcpClientHandlerInfo(Guid.NewGuid()), EchoTcpClientHandler.Handle);
+        }
+
+        private const UInt16 DefaultEchoPort = 7;
+    }
+}
