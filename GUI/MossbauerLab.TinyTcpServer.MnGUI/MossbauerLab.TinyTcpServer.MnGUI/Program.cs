4993f6dd0b569737786d84994ad1ab2882fdbe68
Michael Ushakov <um.nix.user@gmail.com>
2017-07-18 10:42:20 +0400

    Added libraries, some refactoring

--- GUI/MossbauerLab.TinyTcpServer.MnGUI/MossbauerLab.TinyTcpServer.MnGUI/Program.cs
+++ GUI/MossbauerLab.TinyTcpServer.MnGUI/MossbauerLab.TinyTcpServer.MnGUI/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using MossbauerLab.TinyTcpServer.MnGUI.View.Forms;
 
 namespace MossbauerLab.TinyTcpServer.MnGUI
 {
---
+++ GUI/MossbauerLab.TinyTcpServer.MnGUI/MossbauerLab.TinyTcpServer.MnGUI/View/Forms/MainForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MossbauerLab.TinyTcpServer.MnGUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            Load += (sender, args) => FillControls();
+            
+        }
+
+        private void FillControls()
+        {
+            // fill ip address
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    _ipAddressComboBox.Items.Add(ip.ToString());
+            }
+            // load last IP address and TCP Port settings
+            if (File.Exists(ConfigFile))
+            {
+                String[] filelines = File.ReadAllLines(ConfigFile);
+                ApplySettings(GetOptions(filelines));
+            }
+        }
+
+        private IDictionary<String, String> GetOptions(String[] lines)
+        {
+            IDictionary<String, String> options = new Dictionary<String, String>();
+            // todo: parse options ....
+            return options;
+        }
+
+        private void ApplySettings(IDictionary<String, String> options)
+        {
+
+        }
+
+        private const String ConfigFile = @".\settings.txt";
+    }
+}
