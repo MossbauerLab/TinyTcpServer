9403a89327eaf4064047e7ceb97a414fceb002b2
Unknown <um.nix.user@gmail.com>
2017-09-14 01:57:33 +0400

    Server responds back successfully

--- TinyTcpServer/MossbauerLab.TinyTcpServer.Core.FunctionalTests/Server/TestFlexibleTcpServer.cs
+++ TinyTcpServer/MossbauerLab.TinyTcpServer.Core.FunctionalTests/Server/TestFlexibleTcpServer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MossbauerLab.TinyTcpServer.Core.FunctionalTests.TestUtils;
 using MossbauerLab.TinyTcpServer.Core.Server;
 using NUnit.Framework;
 
@@ -11,10 +12,53 @@ namespace MossbauerLab.TinyTcpServer.Core.FunctionalTests.Server
         public void TestScriptRun()
         {
             const String script = @"..\..\TestScripts\SimpleScript.cs";
-            _server = new FlexibleTcpServer(script);
+            _server = new FlexibleTcpServer(script, "127.0.0.1", 8044);
             Boolean result = _server.Start();
             Assert.IsTrue(result, "Checking that result is true");
+            using (TransportClient client = new TransportClient(true, "127.0.0.1", 8044))
+            {
+                client.Open();
+                ExchangeWithRandomDataAndCheck(client, 2055, 1);
+                client.Close();
+            }
             Assert.DoesNotThrow(() => _server.Stop(true), "Checking that server correctly stops");
+        }
+
+        private Byte[] CreateRandomData(Int32 size)
+        {
+            Byte[] randomData = new Byte[size];
+            Random randomGenerator = new Random();
+            for (Int32 counter = 0; counter < randomData.Length; counter++)
+                randomData[counter] = (Byte)randomGenerator.Next(0, 0x0F);
+            return randomData;
+        }
+
+        private void ExchangeWithRandomDataAndCheck(TransportClient client, Int32 dataSize, Int32 repetition, Int32 pauseMin = 0, Int32 pauseMax = 0)
+        {
+            Random pauseRandomGenerator = new Random();
+            for (Int32 repetitionCounter = 0; repetitionCounter < repetition; repetitionCounter++)
+            {
+                SingleExchangeWithRandomDataAndCheck(client, dataSize, repetitionCounter);
+                if (pauseMin > 0 && pauseMax > 0)
+                {
+                    Int32 pause = pauseRandomGenerator.Next(pauseMin, pauseMax);
+                    TimeDelay.Delay(pause);
+                }
+            }
+        }
+
+        private void SingleExchangeWithRandomDataAndCheck(TransportClient client, Int32 dataSize, Int32 cycle)
+        {
+            Byte[] expectedData = CreateRandomData(dataSize);
+            Byte[] actualData = new Byte[expectedData.Length];
+            Int32 bytesReceived;
+            Boolean result = client.Write(expectedData);
+            Assert.IsTrue(result, String.Format("Checking that client successfully write data, at exchange cycle {0}", cycle + 1));
+            result = client.Read(actualData, out bytesReceived);
+            Assert.IsTrue(result, String.Format("Checking that read operation was performed successfully at exchange cycle {0}", cycle + 1));
+            Assert.AreEqual(expectedData.Length, bytesReceived, String.Format("Chechking that client received expected number of bytes at exchange cycle {0}", cycle + 1));
+            for (Int32 counter = 0; counter < expectedData.Length; counter++)
+                Assert.AreEqual(expectedData[counter], actualData[counter], String.Format("Checking that arrays bytes are equals at index {0}, at exchange cycle {1}", counter, cycle + 1));
         }
 
         private ITcpServer _server;
--- TinyTcpServer/MossbauerLab.TinyTcpServer.Core/Server/FlexibleTcpServer.cs
+++ TinyTcpServer/MossbauerLab.TinyTcpServer.Core/Server/FlexibleTcpServer.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.IO;
-using System.Threading;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Reflection;
+using log4net;
 using Microsoft.CSharp;
 
 namespace MossbauerLab.TinyTcpServer.Core.Server
 {
     public class FlexibleTcpServer : TcpServer
     {
-        public FlexibleTcpServer(String scriptFile)
+        public FlexibleTcpServer(String scriptFile, String ipAddress, UInt16 port, ILog logger = null, Boolean debug = false, TcpServerConfig config = null)
+            :base(ipAddress, port, logger, debug, config)
         {
             if(String.IsNullOrEmpty(scriptFile))
                 throw new ArgumentNullException("scriptFile");
@@ -31,16 +32,9 @@ namespace MossbauerLab.TinyTcpServer.Core.Server
             return result;
         }
 
-        public override void Stop(Boolean clearHandlers)
-        {
-            //Terminate();
-            base.Stop(clearHandlers);
-        }
-
         private void Execute()
         {
             String scriptCode = File.ReadAllText(_scriptFile);
-            _cancellationTokenSource = new CancellationTokenSource();
             CompilerResults results = _provider.CompileAssemblyFromSource(_parameters, new String[] {scriptCode});
             if (!results.Errors.HasErrors)
             {
@@ -55,40 +49,12 @@ namespace MossbauerLab.TinyTcpServer.Core.Server
             }
         }
 
-/*        private void Terminate()
-        {
-            if (_state != null && _cancellationTokenSource != null)
-                _cancellationTokenSource.Cancel();
-        }*/
-
-/*        private ScriptOptions GetScriptOptions()
-        {
-            ScriptOptions options = ScriptOptions.Default;
-            // add all references
-            // assemblies
-            options.AddReferences(Assembly.GetAssembly(typeof(TcpServer)));
-            // namespaces
-            options.AddImports("MossbauerLab.TinyTcpServer.Core.Client");
-            options.AddImports("MossbauerLab.TinyTcpServer.Core.Handlers");
-            options.AddImports("MossbauerLab.TinyTcpServer.Core.Handlers.Utils");
-            return options;
-        }*/
-
-        private void SetVariables()
-        {
-            /*if (_state != null)
-            {
-                _state.Variables["_tcpServer"] = this;
-            }*/
-        }
-
-        //private ScriptState<Object> _state;
-        private CSharpCodeProvider _provider = new CSharpCodeProvider(new Dictionary<String, String>()
+        private readonly CSharpCodeProvider _provider = new CSharpCodeProvider(new Dictionary<String, String>()
         {
             {"CompilerVersion", "v4.0"}
         });
 
-        private CompilerParameters _parameters = new CompilerParameters(new []
+        private readonly CompilerParameters _parameters = new CompilerParameters(new[]
         {
             Assembly.GetAssembly(typeof(TcpServer)).Location
         });
@@ -96,6 +62,5 @@ namespace MossbauerLab.TinyTcpServer.Core.Server
         public const String ScriptEntryType = "MossbauerLab.Flexibility.ServerScript";
 
         private readonly String _scriptFile;
-        private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
     }
 }
