26bad1ada48f2573d12149df1e0e3266d66b9185
Unknown <um.nix.user@gmail.com>
2017-09-15 16:16:01 +0400

    Added validator for arguments check

--- Console/MossbauerLab.TinyTcpServer.Console/MossbauerLab.TinyTcpServer.Console/Cli/Data/CommandInfo.cs
+++ Console/MossbauerLab.TinyTcpServer.Console/MossbauerLab.TinyTcpServer.Console/Cli/Data/CommandInfo.cs
@@ -5,10 +5,18 @@ namespace MossbauerLab.TinyTcpServer.Console.Cli.Data
 {
     public class CommandInfo
     {
+        public CommandInfo()
+        {
+            IpAddress = null;
+            Port = null;
+            ScriptFile = null;
+            SettingsFile = null;
+        }
+
         public CommandType Command { get; set; }
 
         public String IpAddress { get; set; }
-        public UInt16 Port { get; set; }
+        public UInt16? Port { get; set; }
         public String ScriptFile { get; set; }
         public String SettingsFile { get; set; }  // server setting file
     }
