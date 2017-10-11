bce5bbc8e682c56b064cf5c4226450f6a37df6e0
Unknown <um.nix.user@gmail.com>
2017-10-12 01:36:11 +0400

    First thougts about replacing switch with state machine

---
+++ Console/MossbauerLab.TinyTcpServer.Console/MossbauerLab.TinyTcpServer.Console/StateMachine/IStateMachine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MossbauerLab.TinyTcpServer.Console.StateMachine
+{
+    public interface IStateMachine
+    {
+        Boolean AddState<T, R>(Func<T, R, Boolean> transitionChecker, Guid stateId, T newState) where T: IComparable; // for Mealy state machine : T is state, R is input
+        Boolean AddState<T>(Func<T, Boolean> transitionChecker, Guid stateId, T newState) where T : IComparable;      // for Moore state machine : T is state, R is input
+        Boolean RemoveState(Guid stateId);
+        void Run(Boolean termonate);
+    }
+}
