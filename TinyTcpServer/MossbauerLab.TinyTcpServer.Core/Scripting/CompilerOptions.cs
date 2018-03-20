3ff8640e3b5c0722ad9eb7b9b96cb98f712ba9d9
Unknown <um.nix.user@gmail.com>
2018-03-21 00:48:26 +0400

    added compiler oprions

---
+++ TinyTcpServer/MossbauerLab.TinyTcpServer.Core/Scripting/CompilerOptions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.CSharp;
+using MossbauerLab.TinyTcpServer.Core.Server;
+
+namespace MossbauerLab.TinyTcpServer.Core.Scripting
+{
+    public class CompilerOptions
+    {
+        public CompilerOptions()
+        {
+            Provider = new CSharpCodeProvider(new Dictionary<String, String>()
+            {
+                {"CompilerVersion", "v4.0"}
+            });
+
+            Parameters = new CompilerParameters(new[]
+            {
+                Assembly.GetAssembly(typeof(TcpServer)).Location
+            });
+
+            ScriptEntryType = DefaultScriptEntryType;
+        }
+
+        public CompilerOptions(CSharpCodeProvider provider, CompilerParameters parameters,
+                               String scriptEntryType = DefaultScriptEntryType)
+        {
+            Provider = provider;
+            Parameters = parameters;
+            ScriptEntryType = scriptEntryType;
+        }
+
+        public String ScriptEntryType { get; set; }
+        public CSharpCodeProvider Provider { get; set; }
+        public CompilerParameters Parameters { get; set; }
+
+        private const String DefaultScriptEntryType = "MossbauerLab.Flexibility.ServerScript";
+    }
+}
